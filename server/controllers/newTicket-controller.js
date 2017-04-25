18119f41a5420ec666fd611154577ec65c13be4c
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-04-25 18:03:34 +0300

    manage server error

--- server/controllers/newTicket-controller.js
+++ server/controllers/newTicket-controller.js
@@ -1,20 +1,21 @@
 module.exports = function (db) {
 
     function get(req, res) {
-        
+        console.log('here1');
     }
 
     function post(req, res) {
+      console.log('here2');
         res.status(201)
             .json({
                 result: {
-                    status: success
+                    status: 'success'
                 }
             });
     }
 
     function put(req, res) {
-       
+       console.log('here3');
     }
 
     return {
