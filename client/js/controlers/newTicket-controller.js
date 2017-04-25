ac06e1b0ee9b30d585c532ec76ee2a6d897c0700
FilipPetkov <filip.petkov@gmail.com>
2017-04-25 15:49:49 +0300

    Form submit object ready

--- client/js/controlers/newTicket-controller.js
+++ client/js/controlers/newTicket-controller.js
@@ -9,26 +9,35 @@ function displayForm() {
             currentDate = currentDate[1] + " " + currentDate[2] + " " + currentDate[3] + " " + currentDate[4];
             $('#main-content').html(template({
                 date: currentDate,
-                name: ["me", "you", "someone else"]
+                name: ["me", "you", "someone else"],
+                urgency: ["low", "mid", "high"]
             }))
         })
     Promise.all([newTicket])
         .then(() => {
             $('#sendNewTicket').on('click', submitForm);
+            $('.dropdown li').on('click', selectOption);
         })
 }
 
+function selectOption() {
+    // console.log($('.dropdown-toggle').dropdown());
+    let currentSelection = $(this).text();
+    $(this).parent().prev().text(currentSelection);
+}
+
 function submitForm() {
-   // console.log($('#shortDecription'));
-    let newTicket={
-        user:$('#userName').text(),
-        shortDescription:$('#shortDescription').val(),
-        longDescription:$('#lognDescription').val(),
-        date:new Date(),
-
-
+    // console.log($('#shortDecription'));
+    let newTicket = {
+        user: $('#userName').text(),
+        shortDescription: $('#shortDescription').val(),
+        longDescription: $('#lognDescription').val(),
+        date: new Date(),
+        engneer: $('#engineer').text(),
+        urgency: $('#urgency').text()
     }
     console.log(newTicket);
+    document.location.href = '#';
 }
 
 export {
