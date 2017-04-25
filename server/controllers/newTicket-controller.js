f4bf89c019d99d5fbf4f0f6990ffd9d3615b5103
FilipPetkov <filip.petkov@gmail.com>
2017-04-25 17:44:16 +0300

    New ticket server controller

---
+++ server/controllers/newTicket-controller.js
@@ -0,0 +1,20 @@
+module.exports = function(db) {
+   
+    function get(req, res) {
+       
+    }
+
+    function post(req, res) {
+    
+    }
+
+    function put(req, res) {
+       
+    }
+
+    return {
+        get: get,
+        post: post,
+        put: put
+    };
+};
