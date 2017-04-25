8ffad38c5156522814e9b4f74adf05d7a783bce5
FilipPetkov <filip.petkov@gmail.com>
2017-04-25 15:14:16 +0300

    Add new ticket form partially working

--- client/js/controlers/newTicket-controller.js
+++ client/js/controlers/newTicket-controller.js
@@ -2,8 +2,33 @@ import * as templates from 'templates';
 import * as data from 'data';
 
 function displayForm() {
-    console.log('yes');
 
+    let newTicket = templates.get('newTicket')
+        .then(function (template) {
+            let currentDate = new Date().toString().split(' ');
+            currentDate = currentDate[1] + " " + currentDate[2] + " " + currentDate[3] + " " + currentDate[4];
+            $('#main-content').html(template({
+                date: currentDate,
+                name: ["me", "you", "someone else"]
+            }))
+        })
+    Promise.all([newTicket])
+        .then(() => {
+            $('#sendNewTicket').on('click', submitForm);
+        })
+}
+
+function submitForm() {
+   // console.log($('#shortDecription'));
+    let newTicket={
+        user:$('#userName').text(),
+        shortDescription:$('#shortDescription').val(),
+        longDescription:$('#lognDescription').val(),
+        date:new Date(),
+
+
+    }
+    console.log(newTicket);
 }
 
 export {
--- client/js/router.js
+++ client/js/router.js
@@ -1,12 +1,13 @@
 import * as users from 'users';
 import * as tickets from 'tickets';
+import * as newTicket from 'newticket';
 
 function initRouter() {
     const router = new Navigo(null, false);
 
     router
         .on('login', () => users.register())
-        .on('NewTicket', () => console.log('here'))
+        .on('NewTicket', () => newTicket.displayForm())
         .on('/', () =>{
             tickets.displayNavBar();
             tickets.displayTickets();
