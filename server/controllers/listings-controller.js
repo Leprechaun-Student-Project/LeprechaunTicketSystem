7b0b90f92ff63a156068da86955f6af2b5698c11
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-04-27 18:35:59 +0300

    add mongodb

--- server/controllers/listings-controller.js
+++ server/controllers/listings-controller.js
@@ -1,31 +1,31 @@
+module.exports = function(db) {
 
-module.exports = function (db) {
-    
     function post_For_Length(request, response) {
         let page_Index = request.params.page_Index;
 
-        let total_Tickets_Length = db.get('tickets')
-            .size()
-            .value()
+        db.collection('tickets').count().then(function(total_Tickets_Length) {
+            console.log('Node says: post for length: ' + total_Tickets_Length);
 
-        console.log('Node says: post for length: ' + total_Tickets_Length);
-
-        response.status(201)
-            .send(total_Tickets_Length);
+            response.status(201)
+                .json({
+                    total_Tickets_Length: total_Tickets_Length
+                });
+        });
     }
 
     function post_For_Tickets(request, response) {
         let page_Index = request.params.page_Index;
         let number_Of_Pages = request.params.number_Of_Pages;
 
-        let array_Of_Tickets = (db.get('tickets')
-            .slice(page_Index * number_Of_Pages, number_Of_Pages + 1)
-            .value()); 
+        db.collection('tickets').find().toArray(function(e, TicketCollection) {
 
-        console.log('Node says: post for Tickets');
-
-        response.status(201)
-            .send(array_Of_Tickets);
+            const array_Of_Tickets = TicketCollection.slice(page_Index * number_Of_Pages, number_Of_Pages + 1);
+            console.log('Node says: post for Tickets ' + JSON.stringify(array_Of_Tickets));
+            response.status(201)
+                .json({
+                    array_Of_Tickets: array_Of_Tickets
+                });
+        });
     }
 
     return {
