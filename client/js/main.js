46fc23ddf8fc88d0cef3080ed14eccd426ea4164
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-05-06 09:53:24 +0300

    remove unnecessary popup

--- client/js/main.js
+++ client/js/main.js
@@ -24,8 +24,3 @@ Handlebars.registerHelper("inc", function (value, options) {
 initUserNavbar();
 
 router.initRouter();
-
-window.alert("Stuff loaded. Now you can use the UI!");
-
-// enable user clicks when the whole stuff is loaded and ready to go
-document.getElementById('no-click').style.display = 'none';
