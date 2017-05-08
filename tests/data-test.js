7938af9e2952cc48fb4c9620f5165b40608acade
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-05-08 19:09:02 +0300

    integrate SystemJS for mocha framework

--- tests/data-test.js
+++ tests/data-test.js
@@ -1,7 +1,11 @@
+import * as data from 'data';
+
 describe('Data Layer Tests', function() {
 
     it('It works', function() {
-        expect(2 + 2).to.be.equal(4);
+        console.log(data.isLoggedIn());
     });
 
 });
+
+mocha.run();
