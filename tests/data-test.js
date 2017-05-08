db8ef7a96bd61c59ee3548e68d9f835315cc93bf
Krasimir Tsvetanov <kvtsvetanov@gmail.com>
2017-05-08 20:02:50 +0300

    add isLoggedIn tests

--- tests/data-test.js
+++ tests/data-test.js
@@ -1,9 +1,26 @@
 import * as data from 'data';
 
-describe('Data Layer Tests', function() {
+const USERNAME_LOCAL_STORAGE_KEY = 'signed-in-user-username',
+    AUTH_KEY_LOCAL_STORAGE_KEY = 'signed-in-user-auth-key';
 
-    it('It works', function() {
-        console.log(data.isLoggedIn());
+const clearLocalStorage = () => {
+    localStorage.removeItem(USERNAME_LOCAL_STORAGE_KEY);
+    localStorage.removeItem(AUTH_KEY_LOCAL_STORAGE_KEY);
+};
+
+describe('Data Layer Tests', () => {
+    beforeEach(clearLocalStorage);
+    afterEach(clearLocalStorage);
+    describe('isLoggedIn Tests', () => {
+        it('Expect isLoggedIn() to return false when AUTH_KEY_LOCAL_STORAGE_KEY is not stored on localhost',
+            () => {
+                expect(data.isLoggedIn()).to.be.false;
+            });
+        it('Expect isLoggedIn() to return true when AUTH_KEY_LOCAL_STORAGE_KEY is stored on localhost',
+            () => {
+                localStorage.setItem(AUTH_KEY_LOCAL_STORAGE_KEY, "valid auth key");
+                expect(data.isLoggedIn()).to.be.true;
+            });
     });
 
 });
